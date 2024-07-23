2f4caf8e97cae6280f1b2cf321744d0b594c7eeb
Tanishk <TanishkSharma795@gmail.com>
2024-07-23 23:11:46 +0530

    fix

--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -21,7 +21,7 @@ const Reservation = () => {
       e.preventDefault();
       try {
         const { data } = await axios.post(
-          "https://backend-psi-nine-40.vercel.app//api/v1/reservation/send",
+          "https://backend-psi-nine-40.vercel.app/api/v1/reservation/send",
           { firstName, lastName, email, phone, date, time },
           {
             headers: {
