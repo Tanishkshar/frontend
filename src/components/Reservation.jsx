5d543d962fdbcf008764618d64ccde1510b03587
Tanishk <TanishkSharma795@gmail.com>
2024-07-23 23:22:13 +0530

    fix

--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -15,32 +15,32 @@ const Reservation = () => {
   const navigate = useNavigate();
 
   const handleReservation = async (e) => {
-    // if (!firstName || !lastName || !email || !date || !phone || !time) {
-    //   toast.error("Pls fill all fields!");
-    // } else {
-    e.preventDefault();
-    try {
-      const { data } = await axios.post(
-        "https://backend-psi-nine-40.vercel.app/api/v1/reservation/send",
-        { firstName, lastName, email, phone, date, time },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-          withCredentials: true,
-        }
-      );
-      toast.success(data.message);
-      setFirstName("");
-      setLastName("");
-      setPhone(0);
-      setEmail("");
-      setTime("");
-      setDate("");
-      navigate("/success");
-    } catch (error) {
-      toast.error(error);
-      navigate("/");
+    if (!firstName || !lastName || !email || !date || !phone || !time) {
+      toast.error("Pls fill all fields!");
+    } else {
+      e.preventDefault();
+      try {
+        const { data } = await axios.post(
+          "https://backend-psi-nine-40.vercel.app/api/v1/reservation/send",
+          { firstName, lastName, email, phone, date, time },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+            withCredentials: true,
+          }
+        );
+        toast.success(data.message);
+        setFirstName("");
+        setLastName("");
+        setPhone(0);
+        setEmail("");
+        setTime("");
+        setDate("");
+        navigate("/success");
+      } catch (error) {
+        toast.error(error);
+      }
     }
   };
 
